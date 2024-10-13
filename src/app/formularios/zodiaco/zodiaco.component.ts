1be3c8ea87f872286a654ad6f1bd099492de6a31
Griselda Ibarra <reyibme@gmail.com>
2024-10-12 19:51:40 -0600

    Zodiaco

--- src/app/formularios/zodiaco/zodiaco.component.ts
+++ src/app/formularios/zodiaco/zodiaco.component.ts
@@ -1,31 +1,80 @@
-export class ZodiacoComponent {
-  nombre: string = '';
-  apellidoPaterno: string = '';
-  apellidoMaterno: string = '';
-  diaNacimiento: number | null = null;
-  mesNacimiento: number | null = null;
-  anioNacimiento: number | null = null;
-  sexo: string = '';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
-  obtenerSignoZodiacalChino(): string {
-    const anio = this.anioNacimiento!;
-    if ([1941, 1953, 1965, 1977, 1989, 2001, 2013, 2025].includes(anio)) return 'Serpiente';
-    if ([1940, 1952, 1964, 1976, 1988, 2000, 2012, 2024].includes(anio)) return 'Dragón';
-    if ([1942, 1954, 1966, 1978, 1990, 2002, 2014, 2026].includes(anio)) return 'Caballo';
-    if ([1943, 1955, 1967, 1979, 1991, 2003, 2015, 2027].includes(anio)) return 'Cabra';
-    if ([1936, 1948, 1960, 1972, 1984, 1996, 2008, 2020].includes(anio)) return 'Rata';
-    if ([1937, 1949, 1961, 1973, 1985, 1997, 2009, 2021].includes(anio)) return 'Buey';
-    if ([1938, 1950, 1962, 1974, 1986, 1998, 2010, 2022].includes(anio)) return 'Tigre';
-    if ([1939, 1951, 1963, 1975, 1987, 1999, 2011, 2023].includes(anio)) return 'Conejo';
-    if ([1944, 1956, 1968, 1980, 1992, 2004, 2016, 2028].includes(anio)) return 'Mono';
-    if ([1945, 1957, 1969, 1981, 1993, 2005, 2017, 2029].includes(anio)) return 'Gallo';
-    if ([1946, 1958, 1970, 1982, 1994, 2006, 2018, 2030].includes(anio)) return 'Perro';
-    if ([1947, 1959, 1971, 1983, 1995, 2007, 2019, 2031].includes(anio)) return 'Cerdo';
-    return 'Signo no encontrado';
+@Component({
+  selector: 'app-zodiaco',
+  standalone: true,
+  templateUrl: './zodiaco.component.html',
+  imports: [ReactiveFormsModule, NgIf],
+})
+export class ZodiacoComponent {
+  zodiacoForm: FormGroup;
+  resultado: any;
+
+  signosChinos: { [key: string]: string } = {
+    'Rata': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftrome.com%2Fhoroscopo%2Fhoroscopo-chino-cual-es-la-leyenda-de-los-doce-animales-del-zodiaco-chino-signos-del-zodiaco-chino-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABAJ',
+    'Buey': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftrome.com%2Fhoroscopo%2Fque-signos-del-horoscopo-chino-tendran-mejor-suerte-durante-el-ano-de-la-rata-de-metal-rata-buey-tigre-conejo-dragon-serpiente-caballo-cabra-mono-gallo-perro-cerdo-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABBe',
+    'Tigre': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Fviu%2Factitud-viu%2Fpredicciones-tigre-horoscopo-chino-2020-ano-de-la-rata-salud-dinero-trabajo-y-amor-tigre-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABAn',
+    'Conejo': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Fviu%2Fpredicciones-conejo-horoscopo-chino-2020-ano-de-la-rata-salud-dinero-trabajo-y-amor-ano-2020-color-de-la-suerte-numero-de-la-suerte-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABAR',
+    'Dragón': 'https://www.istockphoto.com/es/ilustraciones/fechas-del-horoscopo-chino',
+    'Serpiente':'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Fmag%2Fhoroscopo%2Fhoroscopo-chino-2023-predicciones-en-el-amor-para-cada-animal-del-zodiaco-conejo-de-agua-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABBP',
+    'Caballo': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Frespuestas%2Fhoroscopo-chino-2023-color-de-la-suerte-de-cada-signo-en-el-ano-del-conejo-ano-nuevo-chino-2023-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABBm',
+    'Cabra': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Frespuestas%2Fhoroscopo-chino-2023-predicciones-para-el-ano-del-conejo-de-agua-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABAY',
+    'Mono': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Frespuestas%2Fhoroscopo-chino-2023-predicciones-para-el-ano-del-conejo-de-agua-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABBH',
+    'Gallo': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Fviu%2Factitud-viu%2Fgallo-horoscopo-chino-2020-predicciones-para-el-ano-de-la-rata-en-salud-dinero-y-amor-ano-nuevo-chino-nnda-nnnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABAv',
+    'Perro': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Frespuestas%2Fhoroscopo-chino-2023-predicciones-para-el-ano-del-conejo-de-agua-nnda-nnlt-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABA_',
+    'Cerdo': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Fviu%2Factitud-viu%2Fhoroscopo-chino-2019-predicciones-ano-cerdo-ano-nuevo-chino-12-signos-rata-bufalo-tigre-conejo-dragon-gallo-caballo-perro-serpiente-cabra-mono-nnda-nnlt-noticia-590218-noticia%2F&psig=AOvVaw2hDl2xrVmMr-tlsq8cSTwu&ust=1728784242454000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjIkqfdh4kDFQAAAAAdAAAAABAg'
+  };
+
+  constructor(private fb: FormBuilder) {
+    this.zodiacoForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apaterno: ['', Validators.required],
+      amaterno: ['', Validators.required],
+      dia: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+      mes: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
+      anio: ['', [Validators.required, Validators.min(1900)]],
+      sexo: ['', Validators.required],
+    });
   }
 
-  imprimirDatos(): void {
-    const signoZodiacal = this.obtenerSignoZodiacalChino();
-    console.log(`Hola ${this.nombre} ${this.apellidoPaterno} ${this.apellidoMaterno}, tu signo zodiacal chino es: ${signoZodiacal}`);
+  onSubmit() {
+    if (this.zodiacoForm.valid) {
+      const { nombre, apaterno, amaterno, dia, mes, anio } = this.zodiacoForm.value;
+      const edad = this.calcularEdad(dia, mes, anio);
+      const signoZodiacal = this.calcularSignoChino(anio);
+      const imagenSigno = this.obtenerImagenSigno(signoZodiacal);
+
+      this.resultado = {
+        nombreCompleto: `${nombre} ${apaterno} ${amaterno}`,
+        edad,
+        signoZodiacal,
+        imagenSigno,  
+      };
+    }
+  }
+
+  calcularEdad(dia: number, mes: number, anio: number): number {
+    const hoy = new Date();
+    let edad = hoy.getFullYear() - anio;
+    const mesActual = hoy.getMonth() + 1;
+    const diaActual = hoy.getDate();
+
+    if (mes > mesActual || (mes === mesActual && dia > diaActual)) {
+      edad--;
+    }
+
+    return edad;
+  }
+
+  calcularSignoChino(anio: number): string {
+    const cicloZodiaco = (anio - 1900) % 12;
+    const signos = ['Rata', 'Buey', 'Tigre', 'Conejo', 'Dragón', 'Serpiente', 'Caballo', 'Cabra', 'Mono', 'Gallo', 'Perro', 'Cerdo'];
+    return signos[cicloZodiaco];
+  }
+
+  obtenerImagenSigno(signo: string): string {
+    return this.signosChinos[signo] || '';
   }
 }
